83de38576a2ce99f4b3a90435262260571a19890
Scott Cooper <scttcper@gmail.com>
2017-02-25 13:50:52 -0800

    add git config

--- ghpages.sh
+++ ghpages.sh
@@ -7,4 +7,6 @@ fi
 yarn docs
 yarn global add gh-pages
 echo "Pushing to github pages"
-gh-pages -r "https://$PUSH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" -d docs -t
+git config --global user.email "$PUSH_EMAIL"
+git config --global user.name "Travis CI"
+gh-pages -r "https://$PUSH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git" -d docs -x -t
