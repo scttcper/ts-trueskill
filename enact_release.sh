5bc3da47409ffb3019484d6a5bac25391360c35a
Scott Cooper <scttcper@gmail.com>
2017-10-03 09:56:38 -0700

    ugh fix release method

--- enact_release.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-cd ./build
-npm publish
--- stage_release.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -exu
-
-npm run build
-cp package.json build
-cp README.md build
-cp LICENSE build
