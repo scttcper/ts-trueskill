685d3bf65d3e8976566b4d59b39555d41be3dcd1
Scott Cooper <scttcper@gmail.com>
2018-07-02 17:53:55 -0700

    fix: fix package deployment

--- build.ts
+++ build.ts
@@ -4,14 +4,14 @@ import { copySync } from 'fs-extra';
 
 async function build() {
   // copy git folder to dist folder for semantic-release
-  copySync('.git', join(process.cwd(), 'dist/package-dist/.git'));
+  copySync('.git', join(process.cwd(), 'dist/.git'));
   // copy files to distribution folder
   copySync(
     'package.json',
-    join(process.cwd(), 'dist/package-dist/package.json'),
+    join(process.cwd(), 'dist/package.json'),
   );
-  copySync('README.md', join(process.cwd(), 'dist/package-dist/README.md'));
-  copySync('LICENSE', join(process.cwd(), 'dist/package-dist/LICENSE'));
+  copySync('README.md', join(process.cwd(), 'dist/README.md'));
+  copySync('LICENSE', join(process.cwd(), 'dist/LICENSE'));
 }
 
 build()
