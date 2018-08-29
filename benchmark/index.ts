cd5f74b1d6481b1b244568ae7bcc96f5584abab1
Scott Cooper <scttcper@gmail.com>
2018-08-28 22:03:10 -0700

    fix: use microbundle to distribute

--- benchmark/index.ts
+++ benchmark/index.ts
@@ -3,10 +3,7 @@ import * as Benchmark from 'benchmark';
 
 import {
   quality,
-  quality_1vs1,
   rate,
-  rate_1vs1,
-  winProbability,
   Rating,
   TrueSkill,
 } from '../src/public_api';
@@ -23,15 +20,11 @@ function generateTeams(sizes: number[], env?: TrueSkill) {
   });
 }
 
-function generateIndividual(size: number) {
-  return generateTeams(Array(size).fill(1));
-}
-
 const suite = new Benchmark.Suite();
 suite
   .add('5 vs 5', () => {
     const [team1, team2] = generateTeams([5, 5], ts);
-    const rated = rate([team1, team2]);
+    rate([team1, team2]);
   })
   .on('cycle', (event: any) => {
     console.log(String(event.target));
@@ -42,7 +35,7 @@ const suite2 = new Benchmark.Suite();
 suite2
   .add('quality 5 vs 5', () => {
     const [team1, team2] = generateTeams([5, 5], ts);
-    const rated = quality([team1, team2]);
+    quality([team1, team2]);
   })
   .on('cycle', (event: any) => {
     console.log(String(event.target));
