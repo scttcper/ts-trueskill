02ca720289d1fd411b9ea794e43e47316ac8ff82
Scott Cooper <scttcper@gmail.com>
2019-02-23 10:08:40 -0800

    fix: remove exports that are not public

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-export * from './trueskill';
-export * from './rating';
-export * from './mathematics';
-export * from './factorgraph';
+/* eslint-disable @typescript-eslint/camelcase */
+
+export {
+  TrueSkill,
+  calcDrawMargin,
+  rate_1vs1,
+  quality_1vs1,
+  rate,
+  quality,
+  winProbability,
+  expose,
+} from './trueskill';
+export { Rating } from './rating';
